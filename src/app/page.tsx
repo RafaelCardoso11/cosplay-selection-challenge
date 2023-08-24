4f791465995330a10a5815df29e12e209b3084b6
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-08-24 00:27:34 -0300

    fix: removed lenght &&

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -90,7 +90,7 @@ export default function Home() {
       label: "Candidato",
       data: {
         title: "Ficha do Candidato",
-        content: categories.length && <Candidate categories={categories} />,
+        content: <Candidate categories={categories} />,
       },
     },
     {
