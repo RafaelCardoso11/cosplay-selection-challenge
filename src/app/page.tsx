6eefcff05b638bb085cdf6235c90917eef7324a3
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-08-23 22:15:28 -0300

    feat: page form challange selection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,128 @@
-import Image from 'next/image'
+"use client";
+
+import React, { useState } from "react";
+import { Steps } from "primereact/steps";
+import { MenuItem } from "primereact/menuitem";
+import { Candidate } from "./pages/candidate";
+import { Judge } from "./pages/judge";
+
+const categories = [
+  {
+    category: "Mobilidade/Desenvoltura",
+    hasObservation: true,
+    description: "Avaliação da mobilidade e desenvoltura do personagem.",
+    required: true,
+    maxScore: 10,
+    allowDecimal: true,
+    scoreFieldName: "Nota",
+    observationFieldName: "Observação",
+  },
+  {
+    category: "Fidelidade",
+    hasObservation: true,
+    description:
+      "Avaliação da fidelidade em relação à referência original ou ao conceito proposto.",
+    required: true,
+    maxScore: 10,
+    allowDecimal: true,
+    scoreFieldName: "Nota",
+    observationFieldName: "Observação",
+  },
+  {
+    category: "Acabamento",
+    hasObservation: true,
+    description: "Avaliação dos detalhes e acabamento geral da caracterização.",
+    required: true,
+    maxScore: 10,
+    allowDecimal: true,
+    scoreFieldName: "Nota",
+    observationFieldName: "Observação",
+  },
+  {
+    category: "Acessórios (armas/ bolsas/ peruca/ maquiagem)",
+    hasObservation: true,
+    description:
+      "Avaliação dos acessórios, como armas, bolsas, perucas e maquiagem.",
+    required: true,
+    maxScore: 10,
+    allowDecimal: true,
+    scoreFieldName: "Nota",
+    observationFieldName: "Observação",
+  },
+  {
+    category: "Criatividade / Interpretação",
+    hasObservation: true,
+    description:
+      "Avaliação da criatividade e interpretação única do personagem.",
+    required: true,
+    maxScore: 10,
+    allowDecimal: true,
+    scoreFieldName: "Nota",
+    observationFieldName: "Observação",
+  },
+  {
+    category: "Imagem referência",
+    hasObservation: false,
+    description: "Imagens usadas como referência, sem observações associadas.",
+    required: true,
+    maxScore: 10,
+    allowDecimal: true,
+    scoreFieldName: "Nota",
+    observationFieldName: "Observação",
+  },
+];
 
 export default function Home() {
+  const [step, setStep] = useState<number>(0);
+
+  const handleNextPage = () => {
+    setStep((stepCurrent) => stepCurrent + 1);
+  };
+  const items: MenuItem[] = [
+    {
+      label: "Jurado",
+      data: {
+        title: "Ficha do Jurado",
+        content: <Judge handleNextPage={handleNextPage} />,
+      },
+    },
+    {
+      label: "Candidato",
+      data: {
+        title: "Ficha do Candidato",
+        content: categories.length && <Candidate categories={categories} />,
+      },
+    },
+    {
+      label: "Ranking",
+      data: { title: "Ranking dos Candidatos", content: <></> },
+    },
+  ];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <header className="flex items-center justify-center h-40 bg-blue-600  text-white mb-5 p-5">
+        <h1 className="text-2xl font-semibold ">
+          Art Cosplay - Sistema Beta v1.0
+        </h1>
+      </header>
+      <main className=" flex items-center justify-center">
+        <div className="bg-white w-full max-w-lg p-8 rounded-lg shadow-md">
+          <Steps
+            model={items}
+            activeIndex={step}
+            onSelect={(e) => setStep(e.index)}
+            readOnly={false}
+          />
+          <div className="my-10">
+            <h2 className="text-2xl font-semibold mb-4">
+              {items[step].data.title}
+            </h2>
+          </div>
+
+          {items[step].data.content}
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </main>
+    </div>
+  );
 }
