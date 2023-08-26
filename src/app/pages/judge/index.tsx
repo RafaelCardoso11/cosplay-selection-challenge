7f57bb0337c6a3b0699fc027dac2a75f81b53539
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-08-26 13:49:47 -0300

    feat: removed insert file configs in page judge. Here is used default configs by api

--- src/app/pages/judge/index.tsx
+++ src/app/pages/judge/index.tsx
@@ -1,13 +1,13 @@
 "use client";
 import { Input } from "@/components/Input";
 import { Button } from "primereact/button";
-import { Formik, FormikProps } from "formik";
+import { Formik, FormikProps, useFormik } from "formik";
 
 import { toFormikValidationSchema } from "zod-formik-adapter";
 import { JudgeFormSchema, judgeFormSchema, initialValues } from "./validations";
 import { File as FileInput } from "@/components/file";
-import { useState } from "react";
-import { Checkbox } from "primereact/checkbox";
+import { useEffect, useCallback, useRef } from "react";
+import { Toast } from "primereact/toast";
 import axios from "axios";
 
 interface JudgeProps {
@@ -15,98 +15,98 @@ interface JudgeProps {
 }
 
 export const Judge: React.FC<JudgeProps> = ({ handleNextPage }) => {
-  const [isConfigDefault, setIsConfigDefault] = useState<boolean>(false);
-
-  const handleDefaultConfigsFile = async (
-    checked: boolean,
-    propsFormik: FormikProps<JudgeFormSchema>
-  ) => {
-    setIsConfigDefault(checked);
-
-    if (checked) {
-      const basePath = window.location.origin;
-      const configs = await axios.get(`${basePath}/api/config-default`);
-      const blob = new Blob([configs.data], { type: "application/json" });
-
-      const file = new File([blob], "configs.json");
-
-      propsFormik.setFieldValue("configsFile", file);
-    }
-  };
+  const toast = useRef<Toast>(null);
 
   const handleValidation = (values: any) => {
     const errors: any = {};
 
     if (!values.configsFile.name) {
-      errors.configsFile = "Configurações são obrigatórias.";
+      toast.current?.show({
+        severity: "error",
+        summary: "Error",
+        detail: "Configurações são obrigatórias.",
+        life: 3000,
+      });
     }
 
     return errors;
   };
+  const propsFormik = useFormik<JudgeFormSchema>({
+    initialValues,
+    onSubmit: handleNextPage,
+    validationSchema: toFormikValidationSchema(judgeFormSchema),
+    validateOnBlur: false,
+    validateOnChange: false,
+    validate: handleValidation,
+  });
+
+  const { setFieldValue } = propsFormik;
+
+  const handleFetchConfigs = useCallback(async () => {
+    const basePath = window.location.origin;
+    try {
+      const configs = await axios.get(`${basePath}/api/config-default`);
+      return configs;
+    } catch (error) {
+      throw error;
+    }
+  }, []);
+
+  const handleGenerateFileJsonWithConfigs = useCallback(async () => {
+    const configs = await handleFetchConfigs();
+    const blob = new Blob([configs.data], { type: "application/json" });
+
+    const file = new File([blob], "configs.json");
+
+    return file;
+  }, [handleFetchConfigs]);
+
+  useEffect(() => {
+    handleGenerateFileJsonWithConfigs().then((file) => {
+      console.log(file, "fileee");
+      if (file) {
+        setFieldValue("configsFile", file);
+      }
+    });
+  }, [handleGenerateFileJsonWithConfigs, setFieldValue]);
+
   return (
-    <Formik<JudgeFormSchema>
-      initialValues={initialValues}
-      onSubmit={handleNextPage}
-      validationSchema={toFormikValidationSchema(judgeFormSchema)}
-      validateOnBlur={false}
-      validateOnChange={false}
-      validate={handleValidation}
-    >
-      {(props) => (
-        <div>
-          <div className="mb-10">
-            <p className="text-sm text-gray-700">
-              ATENÇÃO JURADOS
-              <br />
-              ANTES DO PARTICIPANTE SUBIR AO PALCO VOCÊ DEVE
-              <br />
-              - MARCAR SEU NOME
-              <br />
-              - ESCREVER O NOME DO PARTICIPANTE
-              <br />
-              - ESCREVER O NOME DO PERSONAGEM
-              <br />
-              <br />
-              TODOS OS PARTICIPANTES TERÃO 30 SEGUNDOS PARA DESFILAR
-              <br />
-              AVALIE SOMENTE APÓS O CANDIDATO DEIXAR O PALCO
-              <br />E SÓ ENVIE O FORMULÁRIO APÓS O OK DA AMANDA.
-            </p>
-          </div>
-          <div className="space-y-5 mb-6">
-            <Input id="judge" label="Nome do Jurado" propsFormik={props} />
-          </div>
-          <div>
-            <div className="flex justify-end align-middle">
-              <label htmlFor="default" className="text-xs pr-2">
-                Configurações Padrões
-              </label>
-              <Checkbox
-                name="default"
-                checked={isConfigDefault}
-                onChange={(e) => {
-                  handleDefaultConfigsFile(e.checked as boolean, props);
-                }}
-              />
-            </div>
-            <FileInput
-              id="configsFile"
-              label="Configurações"
-              accept=".json"
-              inputTextProps={{ disabled: isConfigDefault }}
-              propsFormik={props}
-            />
-          </div>
-          <Button
-            className="w-full justify-center mt-10"
-            onClick={() => {
-              props.submitForm();
-            }}
-          >
-            Confirmar Configurações
-          </Button>
+    <form onSubmit={propsFormik.handleSubmit}>
+      <Toast ref={toast} />
+      <div>
+        <div className="mb-10">
+          <p className="text-sm text-gray-700">
+            ATENÇÃO JURADOS
+            <br />
+            ANTES DO PARTICIPANTE SUBIR AO PALCO VOCÊ DEVE
+            <br />
+            - MARCAR SEU NOME
+            <br />
+            - ESCREVER O NOME DO PARTICIPANTE
+            <br />
+            - ESCREVER O NOME DO PERSONAGEM
+            <br />
+            <br />
+            TODOS OS PARTICIPANTES TERÃO 30 SEGUNDOS PARA DESFILAR
+            <br />
+            AVALIE SOMENTE APÓS O CANDIDATO DEIXAR O PALCO
+            <br />E SÓ ENVIE O FORMULÁRIO APÓS O OK DA AMANDA.
+          </p>
         </div>
-      )}
-    </Formik>
+        <div className="space-y-5 mb-6">
+          <Input id="judge" label="Nome do Jurado" propsFormik={propsFormik} />
+        </div>
+
+        <Button
+          className="w-full justify-center mt-5 bg-blue-600" 
+          type="submit"
+          onClick={() => {
+            propsFormik.submitForm();
+          }}
+        >
+          Confirmar Jurado
+        </Button>
+      </div>
+    </form>
   );
 };
