439602459f7062682b2a2aa3fe19e74d1e6440af
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-08-26 00:17:51 -0300

    feat: candidate with rating

--- src/app/pages/candidate/index.tsx
+++ src/app/pages/candidate/index.tsx
@@ -3,83 +3,142 @@ import { Input } from "@/components/Input";
 import { Accordion, AccordionTab } from "primereact/accordion";
 import { generateArrayFromIndex } from "../../helpers/generateArrayFromIndex";
 import { Category } from "../../interfaces/Category";
-
-import { Separator } from "@/components/separator";
 import { Button } from "primereact/button";
 import { FinalRatings } from "../../../components/finalRatings";
+import { useFormik } from "formik";
+import {
+  CandidateFormSchema,
+  candidateFormSchema,
+  initialValues,
+} from "./validations";
+import { toFormikValidationSchema } from "zod-formik-adapter";
+import { useEffect, useState } from "react";
 
 interface CandidateProps {
   categories: Category[];
 }
 export const Candidate: React.FC<CandidateProps> = ({ categories }) => {
+  const [ratings, setRatings] = useState<{ score: number }[]>([]);
+  const formik = useFormik<CandidateFormSchema>({
+    initialValues,
+    validateOnBlur: false,
+    validateOnChange: false,
+    validationSchema: toFormikValidationSchema(candidateFormSchema),
+    onSubmit: (values: any) => {
+      setRatings([])
+      categories.forEach(({ scoreFieldName }, index) => {
+        setRatings((prev) => [
+          ...prev,
+          { score: values[scoreFieldName.toLowerCase() + index] as number },
+        ]);
+      });
+    },
+    validate: (values: any) => {
+
+      let errors: any = {};
+      categories.forEach(
+        ({ category, scoreFieldName, required, maxScore }, index) => {
+          if (required && values[scoreFieldName.toLowerCase() + index] === "") {
+            errors[
+              scoreFieldName.toLowerCase() + index
+            ] = `A avaliação da categoria "${category}" é obrigatória`;
+          }
+          if (values[scoreFieldName.toLowerCase() + index] > maxScore) {
+            errors[
+              scoreFieldName.toLowerCase() + index
+            ] = `A avaliação deve ser somente até ${maxScore}`;
+          }
+        }
+      );
+      return errors;
+    },
+  });
+  useEffect(() => {
+    categories.forEach(({ scoreFieldName }, index) => {
+      formik.setFieldValue(scoreFieldName.toLowerCase() + index, "");
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [categories]);
+
   return (
-    <div>
-      <div className="space-y-5 mb-6">
-        <Input id="candidato" label="Nome do Candidato" />
-        <Input id="character" label="Personagem" />
-      </div>
-      <h2 className="text-2xl font-semibold mb-1">Avaliação</h2>
-      <p className="mb-7 text-sm break-words">
-        Notas objetivas e observações personalizadas.
-      </p>
+    <form onSubmit={formik.handleSubmit}>
+      <div>
+        <div className="space-y-5 mb-6">
+          <Input
+            id="candidate"
+            label="Nome do Candidato"
+            propsFormik={formik}
+          />
+          <Input id="character" label="Personagem" propsFormik={formik} />
+        </div>
+        <h2 className="text-2xl font-semibold mb-1">Avaliação</h2>
+        <p className="mb-7 text-sm break-words">
+          Notas objetivas e observações personalizadas.
+        </p>
 
-      <Accordion
-        multiple
-        activeIndex={generateArrayFromIndex(categories.length)}
-        className="space-y-5"
-      >
-        {categories.map(
-          (
-            {
-              category,
-              hasObservation,
-              description,
-              observationFieldName,
-              scoreFieldName,
-              maxScore,
-            },
-            index
-          ) => (
-            <AccordionTab
-              header={
-                <>
-                  {index + 1}. {category}
-                </>
-              }
-              key={index}
-            >
-              <div>
-                <h2 className="text-1xl w-full  mb-4">{description}</h2>
-                <div className="space-y-5">
-                  <Input
-                    id={category}
-                    inputTextProps={{
-                      keyfilter: "pnum",
-                    }}
-                    label={scoreFieldName}
-                  />
-                  {hasObservation && (
+        <Accordion
+          multiple
+          activeIndex={generateArrayFromIndex(categories.length)}
+          className="space-y-5"
+        >
+          {categories.map(
+            (
+              {
+                category,
+                hasObservation,
+                description,
+                observationFieldName,
+                scoreFieldName,
+                maxScore,
+              },
+              index
+            ) => (
+              <AccordionTab
+                header={
+                  <>
+                    {index + 1}. {category}
+                  </>
+                }
+                key={index}
+              >
+                <div>
+                  <h2 className="text-1xl w-full  mb-4">{description}</h2>
+                  <div className="space-y-5">
                     <Input
-                      id={category}
-                      label={observationFieldName}
+                      id={scoreFieldName.toLowerCase() + index}
+                      inputTextProps={{
+                        keyfilter: "pnum",
+                        type: "number",
+                        tooltip: maxScore.toString(),
+                      }}
+                      propsFormik={formik}
+                      label={scoreFieldName}
                     />
-                  )}
+                    {hasObservation && (
+                      <Input
+                        id={category + "_obs"}
+                        propsFormik={formik}
+                        label={observationFieldName}
+                      />
+                    )}
+                  </div>
                 </div>
-       
-              </div>
-            </AccordionTab>
-            
-          )
-        )}
-      </Accordion>
+              </AccordionTab>
+            )
+          )}
+        </Accordion>
 
-      <div className="mt-10">
-        <FinalRatings ratings={[{ score: 2 }, { score: 3 }]} />
+        <div className="mt-10">
+          <FinalRatings ratings={ratings} />
+        </div>
+
+        <Button
+          className="w-full justify-center mt-10 bg-blue-700"
+          type="reset"
+        >
+          Confirmar Avaliação
+        </Button>
       </div>
-
-      <Button className="w-full justify-center mt-10 bg-blue-700">
-        Confirmar Avaliação
-      </Button>
-    </div>
+    </form>
   );
 };
