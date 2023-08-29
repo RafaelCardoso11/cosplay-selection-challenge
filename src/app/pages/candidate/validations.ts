2154e99f341b0d97e9b5e1c14e4067170dc06bbc
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-08-29 18:23:40 -0300

    feat: validation ratings

--- src/app/pages/candidate/validations.ts
+++ src/app/pages/candidate/validations.ts
@@ -1,11 +1,20 @@
-import { TypeOf, object, string } from "zod";
+import { TypeOf, array, object, string } from "zod";
 
 export const candidateFormSchema = object({
   candidate: string({
     required_error: "O Nome do Candidato é obrigatório",
   }),
   character: string({
-    required_error: "O Personagem é obrigatório."
+    required_error: "O Personagem é obrigatório.",
+  }),
+  ratings: array(
+    object({
+      value: string({
+        required_error: "Obrigatório",
+      }),
+    })
+  ).refine((value) => value.length > 0, {
+    message: 'O array "ratings" é obrigatório e não pode estar vazio',
   }),
 });
 
@@ -14,4 +23,5 @@ export type CandidateFormSchema = TypeOf<typeof candidateFormSchema>;
 export const initialValues: CandidateFormSchema = {
   candidate: "",
   character: "",
+  ratings: [],
 };
