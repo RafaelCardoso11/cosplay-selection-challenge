ea8dbd96121940cc5a0503568cf9d0bea1d256ee
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-08-31 00:41:57 -0300

    feat: formik from hook useFormik.

--- src/app/pages/candidate/index.tsx
+++ src/app/pages/candidate/index.tsx
@@ -2,7 +2,7 @@
 import { Input } from "@/components/Input";
 import { Category } from "../../interfaces/Category";
 import { Button } from "primereact/button";
-import { FieldArray, FieldArrayRenderProps, Formik, FormikProps } from "formik";
+import { Formik, useFormik } from "formik";
 import {
   CandidateFormSchema,
   candidateFormSchema,
@@ -13,14 +13,17 @@ import { useRef, useState } from "react";
 import { ConfirmPopup, confirmPopup } from "primereact/confirmpopup";
 import { Toast } from "primereact/toast";
 import { Questions } from "./components/questions/questions";
+import { Avaliation } from "@/app/page";
 
 interface CandidateProps {
   categories: Category[];
   handleNextPage: () => void;
+  setAvaliation: React.Dispatch<React.SetStateAction<Avaliation>>
 }
 export const Candidate: React.FC<CandidateProps> = ({
   categories,
   handleNextPage,
+  setAvaliation,
 }) => {
   const toast = useRef<Toast>(null);
   const containerConfirmAvaliationRef = useRef<HTMLDivElement>(null);
@@ -42,8 +45,53 @@ export const Candidate: React.FC<CandidateProps> = ({
       detail: "Finalizar Candidaturas",
       life: 3000,
     });
+
+    setAvaliation({
+      name: formik.values.candidate,
+      character: formik.values.character,
+      totalRating: formik.values.totalRating,
+    });
     handleNextPage();
   };
+
+  const formik = useFormik({
+    initialValues,
+    validateOnBlur: false,
+    validateOnChange: false,
+    validationSchema: toFormikValidationSchema(candidateFormSchema),
+    onSubmit: (_, props) => {
+      confirmPopup({
+        target: containerConfirmAvaliationRef.current
+          ? containerConfirmAvaliationRef.current
+          : undefined,
+        message: "Você deseja continuar a candidatura?",
+        icon: "pi pi-exclamation-triangle",
+        acceptLabel: "Continuar Candidaturas",
+        rejectLabel: "Ver ranking",
+        accept,
+        onHide: () => {
+          setResetAvaliation(true);
+          props.resetForm();
+        },
+        onShow() {
+          setResetAvaliation(false);
+        },
+        reject,
+      });
+    },
+    validate(values) {
+      let errors: any = {};
+      if (values.totalRating === initialValues.totalRating) {
+        errors.totalRating = "A Avaliação não foi preenchida.";
+        toast.current?.show({
+          severity: "error",
+          summary: "Info",
+          detail: errors.totalRating,
+        });
+      }
+      return errors;
+    },
+  });
 
   return (
     <div>
@@ -56,90 +104,48 @@ export const Candidate: React.FC<CandidateProps> = ({
           </span>
         </h2>
       </div>
-      <Formik<CandidateFormSchema>
-        {...{
-          initialValues,
-          validateOnBlur: false,
-          validateOnChange: false,
-          validationSchema: toFormikValidationSchema(candidateFormSchema),
-          onSubmit: (_, props) => {
-            confirmPopup({
-              target: containerConfirmAvaliationRef.current
-                ? containerConfirmAvaliationRef.current
-                : undefined,
-              message: "Você deseja continuar a candidatura?",
-              icon: "pi pi-exclamation-triangle",
-              acceptLabel: "Continuar Candidaturas",
-              rejectLabel: "Ver ranking",
-              accept,
-              onHide: () => {
-                setResetAvaliation(true);
-                props.resetForm();
-              },
-              onShow() {
-                setResetAvaliation(false);
-              },
-              reject,
-            });
-          },
-          validate(values) {
-            let errors: any = {}
-            if (values.totalRating === initialValues.totalRating) {
-              errors.totalRating = "A Avaliação não foi preenchida."
-              toast.current?.show({
-                severity: "error",
-                summary: "Info",
-                detail: errors.totalRating,
-              });
-            }
-            return errors
-          },
-        }}
-      >
-        {(formik) => (
-          <form onSubmit={formik.handleSubmit}>
-            <div>
-              <div className="space-y-5 mb-6">
-                <Input
-                  id="candidate"
-                  label="Nome do Candidato"
-                  propsFormik={formik}
-                />
-                <Input id="character" label="Personagem" propsFormik={formik} />
-              </div>
-              <span className="text-2xl font-semibold mb-1">
-                Avaliação
-                {formik.errors.totalRating && (
-                  <small className="p-error text-sm">*</small>
-                )}
-              </span>
-              <p className="mb-7 text-sm break-words">
-                Notas objetivas e observações personalizadas.
-              </p>
-              <Questions
-                categories={categories}
-                formikProps={formik}
-                resetAvaliation={resetAvaliation}
-              />
 
-              <Toast ref={toast} />
-              <ConfirmPopup />
+      <form onSubmit={formik.handleSubmit}>
+        <div>
+          <div className="space-y-5 mb-6">
+            <Input
+              id="candidate"
+              label="Nome do Candidato"
+              propsFormik={formik}
+            />
+            <Input id="character" label="Personagem" propsFormik={formik} />
+          </div>
+          <span className="text-2xl font-semibold mb-1">
+            Avaliação
+            {formik.errors.totalRating && (
+              <small className="p-error text-sm">*</small>
+            )}
+          </span>
+          <p className="mb-7 text-sm break-words">
+            Notas objetivas e observações personalizadas.
+          </p>
+          <Questions
+            categories={categories}
+            formikProps={formik}
+            resetAvaliation={resetAvaliation}
+          />
 
-              <div
-                className="flex gap-2 justify-center"
-                ref={containerConfirmAvaliationRef}
-              >
-                <Button
-                  className="w-full justify-center mt-10 bg-blue-700"
-                  type="submit"
-                >
-                  Confirmar Avaliação
-                </Button>
-              </div>
-            </div>
-          </form>
-        )}
-      </Formik>
+          <Toast ref={toast} />
+          <ConfirmPopup />
+
+          <div
+            className="flex gap-2 justify-center"
+            ref={containerConfirmAvaliationRef}
+          >
+            <Button
+              className="w-full justify-center mt-10 bg-blue-700"
+              type="submit"
+            >
+              Confirmar Avaliação
+            </Button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
