6bcdf1cc611b20c93fd68ade027eebe4e1b5cc8c
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-09-01 22:47:03 -0300

    feat: add avaliations getter and setters

--- src/app/contexts/avaliation/useAvaliation.ts
+++ src/app/contexts/avaliation/useAvaliation.ts
@@ -2,12 +2,38 @@ import { useContext } from "react";
 import { AvaliationContext } from "./context";
 import { IAvaliation } from "./interface";
 
+const AVALIATIONS_KEY = "avaliations";
+
 export const useAvaliation = () => {
   const { avaliation, setAvaliation } = useContext(AvaliationContext);
 
+  const getAvaliations = () => {
+    const avaliations = localStorage.getItem(AVALIATIONS_KEY);
+
+    return avaliations ? JSON.parse(avaliations) : [];
+  };
+
+  const getLastAvaliation = () => {
+    const avaliationsInString = localStorage.getItem(AVALIATIONS_KEY);
+
+    if (avaliationsInString) {
+      const avaliations: IAvaliation[] = JSON.parse(avaliationsInString);
+
+      const lastAvaliation = avaliations.at(-1);
+      return lastAvaliation as IAvaliation;
+    }
+  };
+
+  const setAvaliations = (avaliations: IAvaliation[]) => {
+    return localStorage.setItem("avaliations", JSON.stringify(avaliations));
+  };
+
+  const resetAvaliations = () => {
+    return localStorage.removeItem(AVALIATIONS_KEY);
+  };
+
   const setValues = (avaliation: IAvaliation | {}) => {
     if (setAvaliation) {
-      console.log(avaliation ,  "avaliationn")
       setAvaliation((prev: IAvaliation) => ({
         ...prev,
         ...avaliation,
@@ -18,5 +44,9 @@ export const useAvaliation = () => {
   return {
     avaliation,
     setValues,
+    setAvaliations,
+    getAvaliations,
+    getLastAvaliation,
+    resetAvaliations,
   };
 };
